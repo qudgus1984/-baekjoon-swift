6977069725affb63fb8b24159872d760517346ce
ByeongHyeon <qudgus1984@naver.com>
2022-03-28 13:57:32 +0900

    1000

---
+++ [baekjoon]swift/1.입출력과사칙연산.swift
@@ -0,0 +1,16 @@
+//
+//  1.입출력과사칙연산.swift
+//  [baekjoon]swift
+//
+//  Created by 이병현 on 2022/03/25.
+//
+
+import Foundation
+
+// 백준 알고리즘 2557
+print("Hello World!")
+
+// 백준 알고리즘 1000
+print("강한친구 대한육군")
+print("강한친구 대한육군")
+
