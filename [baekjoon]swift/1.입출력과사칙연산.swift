a5be4277232c6cdc92a20e7ce879a565a48f584c
ByeongHyeon <qudgus1984@naver.com>
2022-03-28 14:58:35 +0900

    사칙연산 (+,-,*,/) 완료

--- [baekjoon]swift/1.입출력과사칙연산.swift
+++ [baekjoon]swift/1.입출력과사칙연산.swift
@@ -8,9 +8,50 @@
 import Foundation
 
 // 백준 알고리즘 2557
-//rint("Hello World!")
+// print("Hello World!")
+
+// 백준 알고리즘 10718
+// print("강한친구 대한육군")
+// print("강한친구 대한육군")
 
 // 백준 알고리즘 1000
-//print("강한친구 대한육군")
-//print("강한친구 대한육군")
+
+/*
+ let input = readLine()!
+let arr = input.split(separator:" ")
+let a = Int(arr[0])!
+let b = Int(arr[1])!
+print(a+b)
+ */
+
+// 백준 알고리즘 1001
+
+/*
+let input = readLine()!
+let arr = input.split(separator:" ")
+let a = Int(arr[0])!
+let b = Int(arr[1])!
+print(a-b)
+ */
+
+// 백준 알고리즘 10998
+
+/*
+let input = readLine()!
+let arr = input.split(separator:" ")
+let a = Int(arr[0])!
+let b = Int(arr[1])!
+print(a*b)
+*/
+
+// 백준 알고리즘 1008
+
+/*
+ let input = readLine()!
+ let arr = input.split(separator:" ")
+ let a = Double(arr[0])!
+ let b = Double(arr[1])!
+ var value : Double = a / b
+ print(value)
+ */
 
