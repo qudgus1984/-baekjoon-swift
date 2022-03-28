72b3e96eb4f451f67d8e7d7e0b26d744a865d62d
ByeongHyeon <qudgus1984@naver.com>
2022-03-28 16:39:00 +0900

    2. 조건문 14681 완료

--- [baekjoon]swift/main.swift
+++ [baekjoon]swift/main.swift
@@ -2,10 +2,141 @@
 //  main.swift
 //  [baekjoon]swift
 //
-//  Created by 이병현 on 2022/03/25.
+//  Created by 이병현 on 2022/03/28.
 //
 
 import Foundation
 
-print("Hello, World!")
+// 1. 입출려과 사칙연산
+
+// 백준 알고리즘 2557
+// print("Hello World!")
+
+// 백준 알고리즘 10718
+// print("강한친구 대한육군")
+// print("강한친구 대한육군")
+
+// 백준 알고리즘 1000
+
+/*
+ let input = readLine()!
+let arr = input.split(separator:" ")
+let a = Int(arr[0])!
+let b = Int(arr[1])!
+print(a+b)
+ */
+
+// 백준 알고리즘 1001
+
+/*
+let input = readLine()!
+let arr = input.split(separator:" ")
+let a = Int(arr[0])!
+let b = Int(arr[1])!
+print(a-b)
+ */
+
+// 백준 알고리즘 10998
+
+/*
+let input = readLine()!
+let arr = input.split(separator:" ")
+let a = Int(arr[0])!
+let b = Int(arr[1])!
+print(a*b)
+*/
+
+// 백준 알고리즘 1008
+
+/*
+ let input = readLine()!
+ let arr = input.split(separator:" ")
+ let a = Double(arr[0])!
+ let b = Double(arr[1])!
+ var value : Double = a / b
+ print(value)
+ */
+
+//#############################################
+// 2. 조건문
+
+// 백준 알고리즘 1330
+
+/*
+let input = readLine()!
+let arr = input.split(separator:" ")
+let a = Int(arr[0])!
+let b = Int(arr[1])!
+if a > b {
+    print(">")
+}
+else if a < b {
+    print("<")
+}
+else {
+    print("==")
+}
+*/
+
+// 백준 알고리즘 9498
+
+/*
+let grade = Int(readLine()!)!
+if grade >= 90 {
+    print("A")
+}
+else if grade >= 80 {
+    print("B")
+}
+else if grade >= 70 {
+    print("C")
+}
+else if grade >= 60 {
+    print("D")
+}
+else {
+    print("F")
+}
+*/
+
+// 백준 알고리즘 2753
+
+/*
+ let year = Int(readLine()!)!
+if year % 400 == 0 { // 400으로 나누었을 때 나머지가 없는 경우 : 윤년
+    print("1")
+}
+else if year % 100 == 0 { // 100으로 나누었을 때 나머지가 없는 경우 : 윤년이 아님
+    print("0")
+}
+else if year % 4 == 0 { // 4로 나누었을 때 나머지가 없는 경우 : 윤년
+    print("1")
+}
+else { // 이 외의 경우 : 윤년이 아님
+    print("0")
+}
+ */
+
+// 백준 알고리즘 14681
+
+/*
+let x = Int(readLine()!)!
+let y = Int(readLine()!)!
+if (x > 0 && y > 0) {
+    print("1")
+}
+else if (x < 0 && y > 0) {
+    print("2")
+}
+else if (x < 0 && y < 0) {
+    print("3")
+}
+else if (x > 0 && y < 0) {
+    print("4")
+}
+else {
+    print("입력값이 잘못되었습니다.")
+}
+*/
+
 
