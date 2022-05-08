dfb2dca5b8f8da2794105f4a48d42cace8f23246
ByeongHyeon <qudgus1984@naver.com>
2022-05-08 23:56:51 +0900

    4344

--- [baekjoon]swift/main.swift
+++ [baekjoon]swift/main.swift
@@ -513,9 +513,22 @@ for _ in 0 ..< t {
 
 // 백준 알고리즘 4344
 
-let t = Int(readLine()!)!
-for _ in 0 ..< t {
-    let average = readLine()!.map{String ($0)}
+var count: Int = Int(readLine()!)!
+
+for _ in 0..<count{
+    var result: Int = 0
+    var numArr: [Int] = readLine()!.split(separator: " ").map{ Int($0)!}
+    let studentNum: Int = numArr[0]
+    numArr.removeFirst()
+    
+    for i in 0..<studentNum{
+        if numArr[i] > numArr.reduce(0, { $0 + $1}) / studentNum{
+            result += 1
+        }
+    }
+   
+
+    print(String(format: "%.3f", Float(result) / Float(studentNum) * 100),"%",separator: "")
 }
 
 
