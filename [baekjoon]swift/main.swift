4e890de10dee38fa81d5cb62c189264dba0042c2
ByeongHyeon <qudgus1984@naver.com>
2022-05-09 13:20:38 +0900

    switch 공부

--- [baekjoon]swift/main.swift
+++ [baekjoon]swift/main.swift
@@ -513,27 +513,38 @@ for _ in 0 ..< t {
 
 // 백준 알고리즘 4344
 
-var count: Int = Int(readLine()!)!
-
-for _ in 0..<count{
-    var result: Int = 0
-    var numArr: [Int] = readLine()!.split(separator: " ").map{ Int($0)!}
-    let studentNum: Int = numArr[0]
-    numArr.removeFirst()
-    
-    for i in 0..<studentNum{
-        if numArr[i] > numArr.reduce(0, { $0 + $1}) / studentNum{
-            result += 1
-        }
-    }
-   
-
-    print(String(format: "%.3f", Float(result) / Float(studentNum) * 100),"%",separator: "")
-}
+//var count: Int = Int(readLine()!)!
+//
+//for _ in 0..<count{
+//    var result: Int = 0
+//    var numArr: [Int] = readLine()!.split(separator: " ").map{ Int($0)!}
+//    let studentNum: Int = numArr[0]
+//    numArr.removeFirst()
+//
+//    for i in 0..<studentNum{
+//        if numArr[i] > numArr.reduce(0, { $0 + $1}) / studentNum{
+//            result += 1
+//        }
+//    }
+//
+//
+//    print(String(format: "%.3f", Float(result) / Float(studentNum) * 100),"%",separator: "")
+//}
 
 var a: Bool = true
 var b: Bool = false
 print("\(a)\(b)")
+
+var num = 10
+
+switch num {
+case ..<0:
+    print("음수")
+case 0...100:
+    print("99까지")
+default:
+    break
+}
 
 
 
